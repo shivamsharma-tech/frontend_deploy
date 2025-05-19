8d5b6e96bb337cab9e3a29747eb7da28599aca8b
Shivam Sharma <shivamsharma3835@gmail.com>
2025-05-19 11:30:47 +0530

    k

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -109,7 +109,7 @@ export default function Navbar() {
             component="div"
             sx={{ flexGrow: 0.8, color: "purple", fontWeight: 700 }}
           >
-            New Site
+            New WEB Site
           </Typography>
           <Stack
             variant="h5"
