c5837cc7b0d857c1762d776072da399f26f59cde
shivamsharma-tech <shivamsharma3835@gmail.com>
2025-05-20 11:15:50 +0530

    Update Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -109,11 +109,7 @@ export default function Navbar() {
             component="div"
             sx={{ flexGrow: 0.8, color: "purple", fontWeight: 700 }}
           >
-<<<<<<< Updated upstream
-            now this is run on version_v1
-=======
             now this is run on old_version_v1
->>>>>>> Stashed changes
           </Typography>
           <Stack
             variant="h5"
